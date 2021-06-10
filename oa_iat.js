e137a1812333b3c46f7e5ff12901c60d9d16ec67
jbeadlebc <82618660+jbeadlebc@users.noreply.github.com>
2021-06-10 10:18:51 -0700

    Update oa_iat.js

--- oa_iat.js
+++ oa_iat.js
@@ -1,5 +1,20 @@
 define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
-    var API = new APIConstructor(), fullscreen:true;
+    var API = new APIConstructor();
+	
+		var iatObj =
+		{
+			fullscreen:true, //Should we show the task in full screen? A Qualtrics-only feature because in the usual Minno, we can go full-screen right at the beginning of the study.
+        
+			isTouch:false, //Set whether the task is on a touch device.
+			//Set the canvas of the task
+			canvas : {
+				maxWidth: 725,
+				proportions : 0.7,
+				background: '#ffffff',
+				borderWidth: 5,
+				canvasBackground: '#ffffff',
+				borderColor: 'lightblue'
+			},
 
 	return iatExtension({
 		category1 : {
