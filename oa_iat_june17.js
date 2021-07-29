a5b14d3dc1eb809d29ad44400e04116bbb44a232
jbeadlebc <82618660+jbeadlebc@users.noreply.github.com>
2021-07-29 13:11:23 -0700

    Update oa_iat_june17.js

--- oa_iat_june17.js
+++ oa_iat_june17.js
@@ -41,14 +41,6 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 		base_url : {//Where are your images at?
 			image : 'https://jbeadlebc.github.io/pictures_j17/'
 		},
-		fullscreen:true
-		},
-			    
-		leftKeyText :'Press "E" for'
-		},
-       
-		keysCss : {'font-size':'0.9em', 'font-family':'courier', color:'#000000'}
-		
-		
+		fullscreen:true	
 	});
 });
