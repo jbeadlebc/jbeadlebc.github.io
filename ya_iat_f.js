219069466a7a012a792ff0670e29c8c1d41f3b0a
jbeadlebc <82618660+jbeadlebc@users.noreply.github.com>
2021-07-30 12:11:18 -0700

    Update ya_iat_f.js

--- ya_iat_f.js
+++ ya_iat_f.js
@@ -10,12 +10,12 @@ define(['pipAPI', 'https://jbeadlebc.github.io/iat_long.js'], function(APIConstr
  				height : 4 //Used to position the "Or" in the combined block.
  			}, 
  			stimulusMedia : [ //Stimuli content as PIP's media objects
-     		    {image : 'OA1F_HA.jpg'}, 
-     			{image : 'OA2F_HA.jpg'}, 
-     			{image : 'OA3F_HA.jpg'}, 
-     			{image : 'OA1M_HA.jpg'}, 
-     			{image : 'OA2M_HA.jpg'}, 
-     			{image : 'OA3M_HA.jpg'}
+     		    {image : 'YA1F_HA.jpg'}, 
+     			{image : 'YA2F_HA.jpg'}, 
+     			{image : 'YA3F_HA.jpg'}, 
+     			{image : 'YA1M_HA.jpg'}, 
+     			{image : 'YA2M_HA.jpg'}, 
+     			{image : 'YA3M_HA.jpg'}
  			], 
  			//Stimulus css (style)
  			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -28,12 +28,12 @@ define(['pipAPI', 'https://jbeadlebc.github.io/iat_long.js'], function(APIConstr
  				height : 4 //Used to position the "Or" in the combined block.
  			}, 
  			stimulusMedia : [ //Stimuli content as PIP's media objects
-     		    {image : 'OA1F.jpg'}, 
-     			{image : 'OA2F.jpg'}, 
-     			{image : 'OA3F.jpg'}, 
-     			{image : 'OA1M.jpg'}, 
-     			{image : 'OA2M.jpg'}, 
-     			{image : 'OA3M.jpg'}			], 
+     		    {image : 'YA1F.jpg'}, 
+     			{image : 'YA2F.jpg'}, 
+     			{image : 'YA3F.jpg'}, 
+     			{image : 'YA1M.jpg'}, 
+     			{image : 'YA2M.jpg'}, 
+     			{image : 'YA3M.jpg'}			], 
  			//Stimulus css
  			stimulusCss : {color:'#31940F','font-size':'1.8em'}
  		},	
